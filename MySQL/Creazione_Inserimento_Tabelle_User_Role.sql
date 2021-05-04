212ed23a001fd8a30338cb567d046e3a96d57200
Pengfei Chen <penfi_chen@hotmail.com>
2021-05-04 23:25:56 +0200

    Creazione inserimento tabelle User Authorities

--- MySQL/Creazione_Inserimento_Tabelle_User_Role.sql
+++ MySQL/Creazione_Inserimento_Tabelle_User_Role.sql
@@ -1,39 +1,109 @@
-create database questionario;
-use questionario;
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: questionario
+-- ------------------------------------------------------
+-- Server version	8.0.23
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `username` varchar(50) NOT NULL,
-  `password` char(68) NOT NULL,
-  `enabled` tinyint(1) NOT NULL,
-  `nome` varchar(50) NOT NULL,
-  `cognome` varchar(50) NOT NULL,
-  `email` varchar(50) UNIQUE,
-  `eta` int,
-  `genere` char(1),
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-INSERT INTO `users` 
-VALUES 
-('john','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1, 'Jonathan', 'Smile', 'jonathan@questionario.com', 25, 'M'),
-('mary','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1, 'Mary', 'Smith', 'mary@questionario.com', 35, 'F'),
-('susan','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1, 'Susan', 'White', 'susan@questionario.com', NULL, 'F');
-
+--
+-- Table structure for table `authorities`
+--
 
 DROP TABLE IF EXISTS `authorities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `authorities` (
   `username` varchar(50) NOT NULL,
   `authority` varchar(50) NOT NULL,
   UNIQUE KEY `authorities_idx_1` (`username`,`authority`),
   CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
 -- Dumping data for table `authorities`
+--
 
-INSERT INTO `authorities` 
-VALUES 
-('john','ROLE_UTENTE'),
-('mary','ROLE_UTENTE'),
-('mary','ROLE_ADMIN'),
-('susan','ROLE_UTENTE');
+LOCK TABLES `authorities` WRITE;
+/*!40000 ALTER TABLE `authorities` DISABLE KEYS */;
+INSERT INTO `authorities` VALUES ('arianna','ROLE_UTENTE'),('john','ROLE_UTENTE'),('josh','ROLE_UTENTE'),('key','ROLE_UTENTE'),('mary','ROLE_ADMIN'),('mary','ROLE_UTENTE'),('susan','ROLE_UTENTE');
+/*!40000 ALTER TABLE `authorities` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sessioni`
+--
+
+DROP TABLE IF EXISTS `sessioni`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sessioni` (
+  `id_sessione` int NOT NULL AUTO_INCREMENT,
+  `inizio` datetime NOT NULL,
+  `fine` datetime NOT NULL,
+  `nomesessione` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_sessione`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sessioni`
+--
+
+LOCK TABLES `sessioni` WRITE;
+/*!40000 ALTER TABLE `sessioni` DISABLE KEYS */;
+INSERT INTO `sessioni` VALUES (1,'2021-05-01 08:00:00','2021-07-31 18:00:00','Summer splash'),(2,'2021-06-01 09:00:00','2021-06-30 19:00:00','Giugno Fever'),(3,'2021-05-01 08:00:00','2021-05-31 18:00:00','May Day Power');
+/*!40000 ALTER TABLE `sessioni` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `password` char(68) NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `cognome` varchar(50) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `eta` int DEFAULT NULL,
+  `genere` char(1) DEFAULT NULL,
+  PRIMARY KEY (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('arianna','$2a$10$QMFQyIqELVDUFIZBjgHcjuhFHN8Asoqen.6k.skFXTmlPk8km6ZMO',1,'Arianna','Grande','arianna@questionario.com',18,'f'),('john','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1,'Jonathan','Smile','jonathan@questionario.com',25,'M'),('josh','$2a$10$z9LlW/mJ4RpwFvMu8zsFEu0QmmOeVQD9rDHvaTV14bMGIP4X0cg9i',1,'Joshaser','White','joshaser@questionario.com',24,NULL),('key','$2a$10$9091oDsLftp8qvyHBP2Vu.uPDYfgFiWt7/n7/6U8KzgXjNwyFQaiS',1,'Kibum','Kim','kibum@questionario.com',28,'M'),('mary','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1,'Mary','Smith','mary@questionario.com',35,'F'),('susan','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1,'Susan','White','susan@questionario.com',NULL,'F');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-05-04 23:24:12
